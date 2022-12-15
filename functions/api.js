4abf59530608a5baefa151c4afb2904eca9c0a3b
Damian Peralta <damii-peralta@hotmail.com>
2022-12-15 11:06:01 -0300

    finished serverless netlify

---
+++ functions/api.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const serverless = require("serverless-http");
+
+const app = express();
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        'path' : 'Home',
+        'firstName': 'Bibek',
+        'lastName':'Saha'
+    });
+});
+
+router.get('/json', (req, res) => {
+    res.json({
+        'path': 'json',
+        'author': 'Bibek Saha'
+    });
+});
+app.use('/', router);
+module.exports.handler = serverless(app);
